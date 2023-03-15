19115ed97498cd8fac7670d77b06a20856956503
Ngoc Nguyen <nnpk0410@gmail.com>
2023-03-15 14:46:22 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random() * 10);
+
+let num3 = 9;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max( 2,4,6,20,10,11);
